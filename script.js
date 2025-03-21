d0dd961635abcc8bd92855322ea30b08b0732b1b
lydiamatz <lmatziaraki@gmail.com>
2025-03-21 11:24:05 +0200

    Add the basic search engine

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("hello world");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let SearchInput = document.querySelector("#search-form-input");
+  console.log(SearchInput.value);
+}
+
+let SearchFormElement = document.querySelector("#search-form");
+SearchFormElement.addEventListener("submit", handleSearchSubmit);

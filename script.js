53a33cf4f4e0a935f52efbfc01ed064d2ca9bb9f
lydiamatz <lmatziaraki@gmail.com>
2025-03-25 19:34:17 +0200

    Injected more weather data

--- script.js
+++ script.js
@@ -4,9 +4,41 @@ let SearchInput = document.querySelector("#search-form-input");
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
-  temperatureElement.innerHTML = Math.round(temperature);
   CityElement.innerHTML = SearchInput.value;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#time");
+  console.log(response.data.time);
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
 }
+
+function formatDate(date) {
+  let day = date.getDay();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+}
+
+return `${day} ${hours}:${minutes}`;
 
 function handleSearchSubmit(event) {
   event.preventDefault();
